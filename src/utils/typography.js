338876b67681aac950ec5ada3b77f646ae28c53b
Adrian Perea <adrianmarkperea@gmail.com>
2020-05-05 16:26:51 +0900

    Change typewriter theme

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,17 +1,10 @@
 import Typography from "typography"
 import Wordpress2016 from "typography-theme-wordpress-2016"
+import stAnnesTheme from "typography-theme-st-annes"
 
-Wordpress2016.overrideThemeStyles = () => {
-  return {
-    "a.gatsby-resp-image-link": {
-      boxShadow: `none`,
-    },
-  }
-}
+stAnnesTheme.baseFontSize = `20px`
 
-delete Wordpress2016.googleFonts
-
-const typography = new Typography(Wordpress2016)
+const typography = new Typography(stAnnesTheme)
 
 // Hot reload typography in development.
 if (process.env.NODE_ENV !== `production`) {
