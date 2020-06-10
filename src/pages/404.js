1086ac8b73ba4dc070a805a0bc854e6406962dbc
Adrian Perea <adrianmarkperea@gmail.com>
2020-06-11 03:57:13 +0900

    Add 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,29 +1,26 @@
 import React from "react"
-import { graphql } from "gatsby"
-
-import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { Link } from "gatsby"
+import notFoundStyles from "../css/404.module.css"
 
-const NotFoundPage = ({ data, location }) => {
-  const siteTitle = data.site.siteMetadata.title
-
+const NotFoundPage = () => {
   return (
-    <Layout location={location} title={siteTitle}>
+    <>
       <SEO title="404: Not Found - Adrian Perea" />
-      <h1>Not Found</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    </Layout>
+      <div className={notFoundStyles.background}>
+        <div className={notFoundStyles.cta}>
+          <h1>404</h1>
+          <h4>Let it go. It's lost in the void.</h4>
+          <p>
+            It's unsafe here, <br /> let's get you back home.
+          </p>
+          <Link to="/">
+            <button>To Safety</button>
+          </Link>
+        </div>
+      </div>
+    </>
   )
 }
 
 export default NotFoundPage
-
-export const pageQuery = graphql`
-  query {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-  }
-`
