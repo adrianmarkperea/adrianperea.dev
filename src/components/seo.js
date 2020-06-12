83441f7674d779c41d51e07d009d4efad712decb
Adrian Perea <adrianmarkperea@gmail.com>
2020-06-12 15:22:40 +0900

    Add default twitter image

--- src/components/seo.js
+++ src/components/seo.js
@@ -2,6 +2,7 @@ import React from "react"
 import PropTypes from "prop-types"
 import Helmet from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
+import twitterImage from "../../content/assets/favicon.png"
 
 const SEO = ({ description, lang, meta, title }) => {
   const { site } = useStaticQuery(
@@ -61,6 +62,10 @@ const SEO = ({ description, lang, meta, title }) => {
         {
           name: `twitter:description`,
           content: metaDescription,
+        },
+        {
+          name: `twitter:image`,
+          content: twitterImage,
         },
       ].concat(meta)}
     />
